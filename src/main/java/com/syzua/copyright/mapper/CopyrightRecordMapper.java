aabbd62ec3d55e465899109830505b940213fdf4
syzua <2191870080@qq.com>
2026-08-29 22:58:01 +0800

    Add src/main/java/com/syzua/copyright/mapper/CopyrightRecordMapper.java

---
+++ src/main/java/com/syzua/copyright/mapper/CopyrightRecordMapper.java
@@ -0,0 +1,7 @@
+package com.syzua.copyright.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syzua.copyright.entity.CopyrightRecord;
+
+public interface CopyrightRecordMapper extends BaseMapper<CopyrightRecord> {
+}
